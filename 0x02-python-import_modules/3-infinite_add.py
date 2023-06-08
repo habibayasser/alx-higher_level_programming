9f21f3177f88952a38fdc0012be1b1d5cf6a52b9
habiba <habibayasser100@gmail.com>
2023-06-08 14:40:26 -0700

    done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,13 +2,10 @@
 
 if __name__ == "__main__":
     """ prints the result of the addition of all arguments"""
-
-
     import sysy
 
     counter = 0
 
     for i in range(len(sys.argv - 1)):
         counter += int(sys.argv[i + 1]
-
      print("{}".format(counter))
