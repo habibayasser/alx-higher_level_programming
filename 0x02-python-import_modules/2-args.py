aca6d67a85fbcf383d818ca438cbb882476ab53d
habiba <habibayasser100@gmail.com>
2023-06-08 14:17:34 -0700

    done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the number of argumentsand of the list """
+
+    import sys
+
+    counter = len(sys.argv) - 1
+
+    if counter == 0:
+        print("0 arguments.")
+    elif counter == 1:
+        print("1 arguments:")
+    else:
+        print("{} arguments".format(counter))
+    for i in counter:
+        print("{} : {}".format(i + 1, sys.argv [i + 1]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """ prints the result of the addition of all arguments"""
+
+
+    import sysy
+
+    counter = 0
+
+    for i in range(len(sys.argv - 1)):
+        counter += int(sys.argv[i + 1]
+
+     print("{}".format(counter))
