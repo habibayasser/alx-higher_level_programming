24b32d70737245586b5f0deb0dd4cf2c3f1f79d4
habiba <habibayasser100@gmail.com>
2023-06-08 14:29:18 -0700

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,8 +2,7 @@
 
 if __name__ == "__main__":
     """Print sum, diff, mult, div of 10 and 5"""
-
-    from calculator_1.py import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
